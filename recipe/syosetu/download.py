96fe90d7579143f4184200f883ff89a766d3b6a4
Kerry Su <me@sshockwave.net>
2022-06-29 17:03:32 +0800

    Refactor(syosetu) display metadata in <small>

--- recipe/syosetu/download.py
+++ recipe/syosetu/download.py
@@ -168,10 +168,11 @@ class SyosetuNovel(BaseNovel):
         title.string = self.title
         soup.append(title)
 
-        soup.append(soup.new_string('作者：'))
+        soup.append(meta := soup.new_tag('small'))
+        meta.append(soup.new_string('【　作者　】'))
         author = soup.new_tag('a', attrs={'href': self.author_link})
         author.string = self.author
-        soup.append(author)
+        meta.append(author)
 
         details = soup.new_tag('details')
         summary = soup.new_tag('summary')
@@ -353,38 +354,40 @@ class KakuyomuNovel(BaseNovel):
         blockq.string = self.catchpharse
         blockq.attrs['style'] = self.catchphrase_style
 
-        soup.append('【　作者　】')
+        soup.append(meta := soup.new_tag('small'))
+
+        meta.append('【　作者　】')
         author = soup.new_tag('a', attrs={'href': self.author_link})
         author.string = self.author
-        soup.append(author)
-        soup.append(soup.new_tag('br'))
+        meta.append(author)
+        meta.append(soup.new_tag('br'))
 
-        soup.append('【ジャンル】')
+        meta.append('【ジャンル】')
         genre = soup.new_tag('a', attrs={'href': self.genre_link})
         genre.string = self.genre
-        soup.append(genre)
-        soup.append(soup.new_tag('br'))
+        meta.append(genre)
+        meta.append(soup.new_tag('br'))
 
         if self.atten_list:
-            soup.append('【　注意　】' + ' / '.join(self.atten_list))
-            soup.append(soup.new_tag('br'))
+            meta.append('【注意事項】' + ' / '.join(self.atten_list))
+            meta.append(soup.new_tag('br'))
 
         if self.tag_list:
             first = True
-            soup.append('【　タグ　】')
+            meta.append('【　タグ　】')
             for t in self.tag_list:
                 if first:
                     first = False
                 else:
-                    soup.append(' / ')
+                    meta.append(' / ')
                 tag_href = soup.new_tag('a', attrs={'href': t['url']})
-                soup.append(tag_href)
+                meta.append(tag_href)
                 tag_href.string = t['name']
-            soup.append(soup.new_tag('br'))
+            meta.append(soup.new_tag('br'))
 
         soup.append(details := soup.new_tag('details'))
         details.append(summary := soup.new_tag('summary'))
-        summary.string = '【紹介】'
+        summary.string = '紹介'
         migrate_children(self.intro, details)
 
         dl = None
