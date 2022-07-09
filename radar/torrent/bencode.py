be0849c7a7fc3f1d031d7c77f506661729d6e93e
Kerry Su <me@sshockwave.net>
2022-07-09 21:19:38 +0800

    Feat(torrent) add watch to repo

---
+++ radar/torrent/bencode.py
@@ -0,0 +1,100 @@
+from typing import List
+
+class BTFailure(Exception):
+    pass
+
+
+def decode_int(x, f):
+    f += 1
+    newf = x.index(ord('e'), f)
+    n = int(x[f:newf])
+    if x[f] == ord('-'):
+        if x[f + 1] == ord('0'):
+            raise ValueError
+    elif x[f] == ord('0') and newf != f+1:
+        raise ValueError
+    return (n, newf+1)
+
+def decode_string(x, f):
+    colon = x.index(b':', f)
+    n = int(x[f:colon])
+    if x[f] == ord('0') and colon != f+1:
+        raise ValueError
+    colon += 1
+    def try_decode(s):
+        try:
+            return s.decode('utf-8')
+        except:
+            return s
+    return (try_decode(x[colon:colon+n]), colon+n)
+
+def decode_list(x, f):
+    r, f = [], f + 1
+    while x[f] != ord('e'):
+        v, f = decode_func[x[f]](x, f)
+        r.append(v)
+    return (r, f + 1)
+
+def decode_dict(x, f):
+    r, f = {}, f+1
+    while x[f] != ord('e'):
+        k, f = decode_string(x, f)
+        r[k], f = decode_func[x[f]](x, f)
+    return (r, f + 1)
+
+decode_func = dict()
+decode_func['l'] = decode_list
+decode_func['d'] = decode_dict
+decode_func['i'] = decode_int
+decode_func.update({str(i): decode_string for i in range(10)})
+decode_func = {ord(k): v for k, v in decode_func.items()}
+
+def bdecode(x: bytes):
+    r, l = decode_func[x[0]](x, 0)
+    assert l == len(x), "invalid bencoded value (data after valid prefix)"
+    return r
+
+def encode_int(x: int, r: List[bytes]):
+    r += b'i', str(x).encode('utf-8'), b'e'
+
+def encode_bool(x: bool, r: List[bytes]):
+    encode_int(int(x), r)
+
+def encode_string(x: str, r: List[bytes]):
+    encode_bytes(x.encode('utf-8'), r)
+
+def encode_list(x: list, r: List[bytes]):
+    r.append(b'l')
+    for i in x:
+        encode_any(i, r)
+    r.append(b'e')
+
+def encode_dict(x: dict, r: List[bytes]):
+    r.append(b'd')
+    ilist = list(x.items())
+    ilist.sort()
+    for k, v in ilist:
+        encode_string(k, r)
+        encode_any(v, r)
+    r.append(b'e')
+
+def encode_bytes(x: bytes, r: List[bytes]):
+    r += str(len(x)).encode('utf-8'), b':', x
+
+def encode_any(x, r: List[bytes]):
+    encode_func[type(x)](x, r)
+
+encode_func = {
+    int: encode_int,
+    str: encode_string,
+    list: encode_list,
+    tuple: encode_list,
+    dict: encode_dict,
+    bool: encode_bool,
+    bytes: encode_bytes,
+}
+
+def bencode(x):
+    r = []
+    encode_any(x, r)
+    return b''.join(r)
---
+++ radar/torrent/main.py
@@ -0,0 +1,141 @@
+from pathlib import Path
+
+
+def get_parser():
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-r', '--repo', type=Path, nargs=1, help='Path to repo')
+    return parser
+
+def size_repr(size):
+    unit = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB']
+    for t in unit:
+        if size < 1024:
+            return f'{size:.2f}'.rstrip('0').rstrip('.') + t
+        else:
+            size /= 1024
+
+class Tree:
+    def __init__(self, name):
+        self.name = name
+        self.son = dict()
+        self.size = 0
+
+    def add_file(self, paths, size):
+        x = self
+        for v in paths:
+            x.size += size
+            nxt = x.son.get(v)
+            if nxt is None:
+                nxt = Tree(v)
+                x.son[v] = nxt
+            x = nxt
+        x.size += size
+
+    def gen_li(self, lines):
+        assert len(self.son) == 0
+        lines.append(f'<li>{self.name} <code title="{self.size}Bytes">{size_repr(self.size)}</code></li>')
+
+    def gen_html(self, lines):
+        assert len(self.son) > 0
+        dirs = []
+        files = []
+        for s in self.son.values():
+            if len(s.son) == 0:
+                files.append(s)
+            else:
+                dirs.append(s)
+        lines.append('<details>')
+        lines.append(f'<summary>{self.name} <code>{size_repr(self.size)}</code></summary>')
+        lines.append('<ul>')
+        for d in dirs:
+            lines.append('<li>')
+            d.gen_html(lines)
+            lines.append('</li>')
+        for f in files:
+            f.gen_li(lines)
+        lines.append('</ul>')
+        lines.append('</details>')
+
+class TorrentRepo:
+    def __init__(self, base_path: Path):
+        self.path = base_path
+
+    @staticmethod
+    def get_infohash(info):
+        import hashlib
+        from bencode import bencode
+        return hashlib.sha1(bencode(info)).hexdigest()
+
+    def add_raw_torrent(self, file_content, rel_path=Path('.')):
+        from bencode import bdecode, bencode
+        data = bdecode(file_content)
+        infohash = self.get_infohash(data['info'])
+        file_path = self.path / rel_path / infohash
+        with open(file_path.with_suffix('.torrent'), 'wb') as f:
+            f.write(bencode({'info': data['info']}))
+        meta_path = file_path.with_suffix('.yml')
+        meta = []
+        if meta_path.exists():
+            with open(meta_path, 'r') as f:
+                import yaml
+                meta += yaml.load(f)
+        del data['info']
+        if not any(t == data for t in meta):
+            meta.append(data)
+        with open(meta_path, 'w') as f:
+            import yaml
+            yaml.dump(list(meta), f, allow_unicode=True)
+
+    def scan_watch(self, watch_path=None):
+        if watch_path is None:
+            watch_path = self.path / 'watch'
+        if not watch_path.exists():
+            return
+        import os
+        for root, dirs, files in os.walk(watch_path):
+            root = Path(root)
+            for name in files:
+                if name.endswith('.torrent'):
+                    with open(root / name, 'rb') as f:
+                        self.add_raw_torrent(f.read(), root.relative_to(watch_path))
+                    (root / name).unlink()
+
+    def gen_toc_html(self, torrent_path: Path, torrent_link: str):
+        from bencode import bdecode
+        with open(torrent_path, 'rb') as f:
+            info = bdecode(f.read())['info']
+        assert self.get_infohash(info) == torrent_path.stem
+        root = Tree(f'<a href="{torrent_link}">{info["name"]}</a>')
+        for it in info['files']:
+            root.add_file(it['path'], it['length'])
+        lines = []
+        root.gen_html(lines)
+        return '\n'.join(lines)
+
+    def gen_dir_html(self, abs_path: Path):
+        import os
+        lines = []
+        for root, dirs, files in os.walk(abs_path):
+            root = Path(root)
+            rel_path = root.relative_to(abs_path)
+            first = True
+            for name in sorted(files):
+                if not name.endswith('.torrent'):
+                    continue
+                if first:
+                    first = False
+                    if root != abs_path:
+                        lines.append(f'<h2>{rel_path.as_posix()}</h2>')
+                lines.append(self.gen_toc_html(root / name, (rel_path / name).as_posix()))
+        return '\n'.join(lines)
+
+def main():
+    args = get_parser().parse_args()
+    repo = TorrentRepo(args.repo[0])
+    repo.scan_watch()
+    with open(repo.path / 'README.md', 'w') as f:
+        f.write(repo.gen_dir_html(repo.path))
+
+if __name__ == '__main__':
+    main()
